aec272cb26c446d6934d7e93a6a4081a93656fd2
Sabrina Carrion <SabrinaCarrion43@gmail.com>
2022-09-26 10:16:43 -0500

    moved api into separate page

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,17 +1,6 @@
-const router = require("express").Router();
 const { randomUUID } = require("crypto");
 const { readFromFile, readAndAppend, writeToFile } = require("../helpers/fsUtils");
-const path = require("path");
 const { write } = require("fs");
-
-
-router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
-})
-// wildcard is asterisk for express
-router.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
-})
 
 router.get("/api", (req, res) => {
     readFromFile("./db/db.json").then((data) =>
@@ -38,5 +27,3 @@ router.post("/api/notes", (req, res) => {
 
     }
 });
-
-module.exports = router;
